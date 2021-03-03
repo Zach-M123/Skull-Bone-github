a2a84c68a287c398849650774314c1e74e22e275
Zach-M123 <zmosete@gmail.com>
2021-03-03 08:32:00 +0200

    remote sever and ipaddr

--- networking.py
+++ networking.py
@@ -6,3 +6,8 @@ local_hostname = socket.gethostname()
 local_ipaddr = socket.gethostbyname(local_hostname)
 print("Local Hostname: %s" %local_hostname + "\n" + "Local Ip addr: %s" %local_ipaddr)
 
+# generate remote server ip address using gethkostbyname()
+remote_server = "google.com"
+remote_ipaddr = socket.gethostbyname(remote_server)
+print("Remote Server: %s" %remote_server + '\n' + "Remote Ipaddress: %s" %remote_ipaddr)
+
