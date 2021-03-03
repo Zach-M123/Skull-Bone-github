e1ff9615034e85a84a107b93e75d24171a7407db
Zach-M123 <zmosete@gmail.com>
2021-03-03 08:46:33 +0200

    fucntion()

--- networking.py
+++ networking.py
@@ -1,13 +1,22 @@
 #!/usr/sbin/python3
 import socket
 
-# basic code
-local_hostname = socket.gethostname()
-local_ipaddr = socket.gethostbyname(local_hostname)
-print("Local Hostname: %s" %local_hostname + "\n" + "Local Ip addr: %s" %local_ipaddr)
+# create a fucntion() for Local machine info
+def local_info():
+    local_host = socket.gethostname()
+    local_ipaddr = socket.gethostbyname(local_host)
+    print("Local Hostname: %s" %local_host + '\n' + "Local Ip addr: %s" %local_ipaddr)
 
-# generate remote server ip address using gethkostbyname()
-remote_server = "google.com"
-remote_ipaddr = socket.gethostbyname(remote_server)
-print("Remote Server: %s" %remote_server + '\n' + "Remote Ipaddress: %s" %remote_ipaddr)
+if __name__ == "__main__":
+    local_info()
+
+# create a function() for remote server info
+def server_machine():
+    remote_server = "google.com"
+    remote_ipaddr = socket.gethostbyname(remote_server)
+    print("Remote Server: %s" %remote_server + '\n' + "remote_ipaddr: %s" %remote_ipaddr)
+
+if __name__ == "__main__":
+    server_machine()
+
 
